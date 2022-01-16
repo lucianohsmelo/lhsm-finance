57ca421a0bb22988e71aa488a2fdae2f28f54ce1
Luciano Melo <lucianohsmelo@gmail.com>
2022-01-16 16:57:22 -0300

    Initial commit

---
+++ src/main/services/YahooFinanceService.ts
@@ -0,0 +1,11 @@
+export class YahooFinanceService {
+  private readonly baseUrl: string = 'https://yfapi.net';
+
+  public async quote() {
+    //TO-DO
+  }
+
+  public async spark() {
+    //TO-DO
+  }
+}
